60f88c17d7b058ef05da711ec8ecbed7019677e5
Kinkreux <loicopondoxva@gmail.com>
2018-04-16 23:17:31 +0300

    working on admin and json

---
+++ admin.php
@@ -0,0 +1,31 @@
+<?php
+
+var_dump($_POST);
+var_dump($_FILES);
+
+?>
+
+<html>
+<head>
+    <title>Добро пожаловать на сервис mytests</title>
+</head>
+<body>
+<h1>Добро пожаловать на сервис mytests!</h1>
+<p>Вы можете загрузить файл с тестом в формате .json по следующему шаблону:</p>
+<code>{"problem":
+            {"question":"Текст вопроса",
+            "response":[
+                           {"alternative":"Текст варианта ответа"},
+                           {"valid":0/1}
+                           ]
+           }
+}           "</code>
+<form enctype="multipart/form-data" action="admin.php" method="post">
+    <p>Назовите тест:</p>
+    <input type="text" name="testname">
+    <p>Загрузите файл json:</p>
+    <input type="file" name="testfile">
+    <input type="submit" value="Загрузить тест">
+</form>
+</body>
+</html>
